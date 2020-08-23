679326005a55b910fe0ff52002f42a1bcd3be97c
lukasmortman <lukasbestnu@gmail.com>
2020-08-23 11:16:23 +0200

    added to make text change lines if too long

--- memegenerator/memegenerator.js
+++ memegenerator/memegenerator.js
@@ -1,25 +1,34 @@
 const inputelement = document.querySelector('#inputvalue');
 const inputelement2 = document.querySelector('#inputvalue2');
-let font = 10;
+let font = 25;
 let imagesource = "";
 
 
 
 
  function drawImage(){
-     var ctx = $("canvas")[0].getContext("2d"),
-         img = new Image();
+     var c = document.getElementById('canvas1');
+     var ctx = c.getContext('2d');
+
+     img = new Image();
+    var text = inputelement.value;
+    var text2 = inputelement2.value;
+     var canvasTxt = window.canvasTxt.default;
+    canvasTxt.fontSize = font;
+
 
      img.onload = function(){
          ctx.drawImage(img, 0, 0, 500, 500);
-         ctx.font = font+"px Arial";
-         ctx.fillText(inputelement.value,275,125);
-         ctx.fillText(inputelement2.value,275,375);
+         canvasTxt.drawText(ctx, text, 250, 20, 250, 200);
+         canvasTxt.drawText(ctx, text2, 250, 270, 250, 200);
+
      };
      img.src = imagesource;
  }
 
-
+    function drawimageonclick(){
+     drawImage()
+}
  $("button").click(drawImage);
 
  function DownloadCanvasAsImage(){
@@ -42,12 +51,19 @@ function DecreaeFontSize() {
     font -= 5;
 }
 
+function clearinputvalue(){
+    document.getElementById('inputvalue').value = '';
+    document.getElementById('inputvalue2').value = '';
+}
+
 function SelecteDrake() {
     imagesource = "meme11.png";
+    clearinputvalue();
     drawImage()
 }
 
 function SelecteHonung() {
     imagesource = "meme12.png";
+    clearinputvalue();
     drawImage()
 }
