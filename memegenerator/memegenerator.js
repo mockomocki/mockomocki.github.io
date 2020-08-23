75c45a6cbb62787c70b2f8932ce880fbb387f4de
lukasmortman <lukasbestnu@gmail.com>
2020-08-23 15:50:39 +0200

    yes

--- memegenerator/memegenerator.js
+++ memegenerator/memegenerator.js
@@ -1,5 +1,7 @@
 const inputelement = document.querySelector('#inputvalue');
 const inputelement2 = document.querySelector('#inputvalue2');
+const inputelement3 = document.querySelector('#inputvalue3');
+const inputelement4 = document.querySelector('#inputvalue4');
 let font = 25;
 let imagesource = "";
 
@@ -11,20 +13,34 @@ let imagesource = "";
      var ctx = c.getContext('2d');
 
      img = new Image();
-    var text = inputelement.value;
-    var text2 = inputelement2.value;
+     var text = inputelement.value;
+     var text2 = inputelement2.value;
+     var text3 = inputelement3.value;
+     var text4 = inputelement4.value;
      var canvasTxt = window.canvasTxt.default;
-    canvasTxt.fontSize = font;
+     canvasTxt.fontSize = font;
 
+     if(imagesource === "meme13.png"){
+         img.onload = function(){
+             ctx.drawImage(img, 0, 0, 500, 500);
+             canvasTxt.drawText(ctx, text, 0, -40, 250, 200);
+             canvasTxt.drawText(ctx, text2, 0, 80, 250, 200);
+             canvasTxt.drawText(ctx, text3, 0, 200, 250, 200);
+             canvasTxt.drawText(ctx, text4, 0, 320, 250, 200);
 
-     img.onload = function(){
-         ctx.drawImage(img, 0, 0, 500, 500);
-         canvasTxt.drawText(ctx, text, 250, 20, 250, 200);
-         canvasTxt.drawText(ctx, text2, 250, 270, 250, 200);
+         };
+         img.src = imagesource;
+     }else {
+         img.onload = function(){
+             ctx.drawImage(img, 0, 0, 500, 500);
+             canvasTxt.drawText(ctx, text, 250, 20, 250, 200);
+             canvasTxt.drawText(ctx, text2, 250, 270, 250, 200);
 
-     };
-     img.src = imagesource;
- }
+         };
+         img.src = imagesource;
+     }
+     }
+
 
     function drawimageonclick(){
      drawImage()
@@ -54,17 +70,59 @@ function DecreaeFontSize() {
 function clearinputvalue(){
     document.getElementById('inputvalue').value = '';
     document.getElementById('inputvalue2').value = '';
+    document.getElementById('inputvalue3').value = '';
+    document.getElementById('inputvalue4').value = '';
+}
+
+function showinputfield34(){
+    document.getElementById('inputvalue3').type = 'text';
+    document.getElementById('inputvalue4').type = 'text';
+}
+
+function hideinputfield34(){
+    document.getElementById('inputvalue3').type = 'hidden';
+    document.getElementById('inputvalue4').type = 'hidden';
 }
 
 function SelecteDrake() {
     imagesource = "meme11.png";
     clearinputvalue();
+    hideinputfield34();
     drawImage()
 }
 
 function SelecteHonung() {
     imagesource = "meme12.png";
     clearinputvalue();
+    hideinputfield34();
     drawImage()
 }
 
+function SelecteBrain() {
+    imagesource = "meme13.png";
+    clearinputvalue();
+    showinputfield34();
+    drawImage()
+}
+
+//function drawImage4inputsBrain(){
+//     var c = document.getElementById('canvas1');
+//     var ctx = c.getContext('2d');
+//
+//     img = new Image();
+//     var text = inputelement.value;
+//     var text2 = inputelement2.value;
+//     var text3 = inputelement3.value;
+//     var text4 = inputelement4.value;
+//     var canvasTxt = window.canvasTxt.default;
+//     canvasTxt.fontSize = font;
+//
+//
+//     img.onload = function(){
+//         ctx.drawImage(img, 0, 0, 500, 700);
+//         canvasTxt.drawText(ctx, text, 250, 20, 250, 200);
+//         canvasTxt.drawText(ctx, text2, 250, 270, 250, 200);
+//
+//     };
+//     img.src = imagesource;
+// }
