573cda01dc07ce280c0b79a3fefc09cbaceb1e39
lukasmortman <lukasbestnu@gmail.com>
2020-11-02 18:25:29 +0100

    added stuff

---
+++ league/script.js
@@ -0,0 +1,43 @@
+const cors = "https://cors-anywhere.herokuapp.com/";
+const apikey = "?api_key=RGAPI-dc5fc7a2-c9ec-49db-bf59-14455527ed65";
+const yes= "lol/spectator/v4/active-games/by-summoner/";
+const url = "https://eun1.api.riotgames.com/";
+const getname = "/lol/summoner/v4/summoners/by-name/";
+const imageurl = "https://ddragon.leagueoflegends.com/cdn/10.22.1/img/champion/";
+const nice = "https://ddragon.leagueoflegends.com/cdn/10.22.1/data/en_US/champion.json";
+
+
+const request = async () => {
+        const leaguename = document.getElementById('searchTxt').value;
+        let newurl2 = cors + url + getname + leaguename + apikey;
+        const response = await fetch(newurl2);
+        const json = await response.json();
+        const gameinfo = await fetch(cors + url + yes + json.id + apikey);
+        const json2 = await gameinfo.json();
+        const repsone2 = await fetch(nice);
+        const json3 = await repsone2.json();
+        console.log(json2);
+
+        for(i=0;i<10;i++) {
+                for (var key of Object.keys(json3.data)) {
+                        if ("" + json2.participants[i].championId + "" === json3.data[key].key) {
+                                var image = new Image();
+                                image.src = imageurl +json3.data[key].image.full;
+                                document.getElementById(""+ i +"").appendChild(image);
+                                document.getElementById("" + 0 + i + "").innerText = (json2.participants[i].summonerName)
+                        }
+                }
+        }
+
+
+
+
+
+};
+
+function CheckGameInfo() {
+        request();
+}
+
+
+
